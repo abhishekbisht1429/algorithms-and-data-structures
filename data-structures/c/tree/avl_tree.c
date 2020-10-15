e9d1ed2cb6c036b1b52e62eb0b43ccc3259be868
reckoner1429 <abhishekbisht1429@gmail.com>
2020-10-15 12:19:27 +0530

    data structures - AVL Tree

--- data-structures/c/tree/avl_tree.c
+++ data-structures/c/tree/avl_tree.c
@@ -1,24 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
-struct Node{
+struct Node {
     int key;
-    int h; 
     struct Node *left;
     struct Node *right;
+    int height;
 };
-
-//h variable is storing height of individual node, considering that node as root
-//so its h = 1 + max(height of lef subtree, height of right subtree)
-
 typedef struct Node NODE;
 
-struct AVLTree{
-    NODE* root;
+struct Avl {
+    NODE *root;
 };
 
-typedef struct AVLTree AVL;
+typedef struct Avl AVL;
+
+NODE *newAVLNode(int key) {
+    NODE *node = (NODE*)malloc(sizeof(NODE));
+    node->key = key;
+    node->left = NULL;
+    node->right = NULL;
+    node->height = 1;
+
+    return node;
+}
 
 AVL* newAVLTree(){
     AVL *t = (AVL*) malloc(sizeof(AVL));
@@ -27,239 +32,233 @@ AVL* newAVLTree(){
     return t;
 }
 
-
-//inline this function will sbstitute there where ever we call this 
-static inline int max(int x, int y){
-    return (x>y)?x:y ;
-}
-
-static int height(NODE *t){
-    if(t == NULL){
+static int height(NODE *node) {
+    if(node == NULL) {
         return 0;
+    } else {
+        return node->height;
     }
-    return t->h;
 }
 
-NODE* newAVLNode(int key){
-    NODE *t = (NODE*)malloc(sizeof(NODE));
-    t->key = key;
-    t->h = 1;
-    t->left = NULL;
-    t->right = NULL;
-
-    return t;
-}
-
-static NODE* leftRotation(NODE *t){
-    //if t is null
-    if(t == NULL){
-        return NULL;
-    }
-
-    //assuming that right child of "t" in existing 
-    NODE *temp = t->right;
-    t->right = temp->left;
-    temp->left = t;
-
-    //updating heights of t and temp
-    t->h = 1 + max(height(t->left), height(t->right));
-    temp->h = 1 + max(height(temp->left), height(temp->right));
-
-    return temp;
-}
-
-static NODE* rightRotation(NODE *t){
-    //if t is null
-    if(t == NULL){
-        return NULL;
-    }
-
-    //assuming that left child of "t" in existing
-    NODE *temp = t->left;
-    t->left = temp->right;
-    temp->right = t; 
-
-    //updating heights of t and temp 
-    t->h = 1 + max(height(t->left), height(t->right));
-    temp->h = 1 + max(height(temp->left), height(temp->right));    
-
-    return temp;
-}
-
-static int balanceFactor(NODE *root){
-    if(root == NULL){
+static int balance_factor(NODE *node) {
+    if(node == NULL) {
         return 0;
+    } else {
+        return height(node->left) - height(node->right);
     }
-    return height(root->left) - height(root->right);
 }
 
-static NODE* insert_node(NODE *root, int key){
-    //inserting just like in BST 
-    if(root == NULL){
-        return newAVLNode(key);
-    }
-    if(key>root->key){
-        root->right = insert_node(root->right, key);
-    }
-    else if(key<root->key){
-        root->left = insert_node(root->left, key);
-    }
-    else{
-        return root;
-    }
-
-    //updating height 
-    root->h = 1+max(height(root->left), height(root->right));
-
-    //now checking balance factor
-    int bf = balanceFactor(root);
-
-    if(bf<-1 && key>root->right->key){
-        //case: RR
-        root = leftRotation(root);
-    }
-    else if(bf<-1 && key<root->right->key){
-        //case: RL
-        root->right = rightRotation(root->right);
-        root = leftRotation(root);
-
-    }
-    else if(bf>1 && key>root->left->key){
-        //case: LR
-        root->left = leftRotation(root->left);
-        root = rightRotation(root);
-    }
-    else if(bf>1 && key<root->left->key){
-        //case: LL
-        root = rightRotation(root);
-    }
-    
-
-    return root; 
+static inline int max(int x, int y) {
+    return x>y?x:y;
 }
 
-void avlInsertion(AVL *t, int key){
-    t->root = insert_node(t->root, key);
-}
-
-static void inorderTraversal(NODE *root){
-    if(root == NULL){
-        return;
-    }
-    inorderTraversal(root->left);
-    printf("%d ", root->key);
-    inorderTraversal(root->right);
-}
-
-void inorderAVLTraversal(AVL* t){
-    inorderTraversal(t->root);
-}
-
-static NODE* minval_node(NODE *root, int key){
-    if(root == NULL){
+//NOTE: the caller to this function will ensure that node->right is not null
+static NODE *rotateLeft(NODE *node) {
+    // printf("rotating left %d\n", node->key);
+    if(node == NULL){
         return NULL;
     }
-    else if(root->left == NULL){
-        return root;
-    }
-    return minval_node(root->left, key);
-}
+    NODE *root = node->right;
+    node->right = root->left;
+    root->left = node;
 
-static NODE* delete(NODE *root ,int key){
-    //BST deletion 
-    if(root == NULL){
-        return root;
-    }
-
-    if(key>root->key){
-        root->right = delete(root->right, key);
-    }
-    else if(key<root->key){
-        root->left = delete(root->left, key);
-    }
-    else{
-        //if single child or no child 
-        if(root->right == NULL){
-            NODE *temp = root->left;
-            free(root);
-            root = temp;
-        }
-        else if(root->left == NULL){
-            NODE *temp = root->right;
-            free(root);
-            root = temp;
-        }
-        else{
-            //if two child
-            NODE *suc = minval_node(root->right, key);
-            root->key = suc->key;
-            root->right = delete(root->right, suc->key);
-        }
-        
-    }
-
-    //AVL procedure
-   
-    if(root!=NULL){
-        root->h = 1+max(height(root->left), height(root->right));
-
-        int bf = balanceFactor(root);
-
-        if(bf>1){
-            //deletion from right subtree
-            int bfl = balanceFactor(root->left);
-            if(bfl>=0){
-                root = rightRotation(root);
-            }
-            else{
-                root->left = leftRotation(root->left);
-                root = rightRotation(root);
-            }
-        }
-        else if(bf<-1){
-            //deletion from left subtree
-            int bfr = balanceFactor(root->right);
-            if(bfr<=0){
-                root = leftRotation(root);
-            }
-            else{
-                root->right = rightRotation(root->right);
-                root = leftRotation(root);
-            }
-        }
-    }
+    node->height = max(height(node->left), height(node->right)) + 1;
+    root->height = max(height(root->left), height(root->right)) + 1;
 
     return root;
 }
 
-void avlDeletion(AVL* t, int key){
-    t->root = delete(t->root, key);
+//NOTE: the caller to this function will ensure that node->left is not null
+static NODE *rotateRight(NODE *node) {
+    // printf("rotating right %d\n", node->key);
+    if(node == NULL) {
+        return NULL;
+    }
+
+    NODE *root = node->left;
+    node->left = root->right;
+    root->right = node;
+
+    node->height = max(height(node->left), height(node->right)) + 1;
+    root->height = max(height(root->left), height(root->right)) + 1;
+
+    return root;
 }
 
-// int main(){
+static NODE *insert(NODE *root, int key) {
+    if(root == NULL) {
+        return newAVLNode(key);
+    }
+    if(key < root->key) {
+        // printf("left\n");
+        root->left = insert(root->left, key);
+    } else {
+        // printf("right\n");
+        root->right = insert(root->right, key);
+    }
 
-//     AVL *t = newAVLTree();
+    root->height = max(height(root->left), height(root->right)) + 1;
+
+    int balance = balance_factor(root);
+
+    //left subtree of left child
+    //rotate right
+    if(balance > 1 && key < root->left->key) {
+        root = rotateRight(root);
+    } else if(balance > 1 && key > root->left->key) { 
+        //right subtree of left child --> rotate left child towards left
+        // and then right rotate the root
+        root->left = rotateLeft(root->left);
+        root = rotateRight(root);
+    } else if(balance < -1 && key > root->right->key) {
+        //right subree of right child --> rotate left
+        root = rotateLeft(root);
+    } else if(balance < -1 && key < root->right->key) {
+        //left subtree of right child --> rotate right child towards right
+        //and then rotate the root towards left
+        root->right = rotateRight(root->right);
+        root = rotateLeft(root);
+    }
+    return root;
+}
+
+void avlInsert(AVL *tree, int key) {
+    tree->root = insert(tree->root, key);
+}
+
+static NODE *minVal(NODE *node) {
+    if(node == NULL) {
+        return NULL;
+    }
+    if(node->left == NULL) {
+        return node;
+    } else {
+        return minVal(node->left);
+    }
+}
+
+static NODE *delete(NODE *root, int key) {
+    if(root == NULL) {
+        return NULL;
+    }
+    if(key < root->key) {
+        root->left = delete(root->left, key);
+    } else if(key > root->key) {
+        root->right = delete(root->right, key);
+    } else {
+        if(root->right == NULL) {
+            NODE *temp = root->left;
+            free(root);
+            root = temp;
+        } else if(root->left == NULL) {
+            NODE *temp = root->right;
+            free(root);
+            root = temp;
+        } else {
+            NODE *succ = minVal(root->right); // a -> b == value of b at address stored in a // *a == value at address stored by a
+            root->key = succ->key;
+            root->right = delete(root->right, succ->key);
+        }
+
+        if(root!=NULL) {
+            root->height = 1 + max(height(root->left), height(root->right));
+            int bf_root = balance_factor(root);
+            if(bf_root > 1) { //case R
+                int bf_left = balance_factor(root->left);
+                if(bf_left >= 0) { //R0 and R1 case
+                    root = rotateRight(root);
+                } else { //R-1 case
+                    root->left = rotateLeft(root->left);
+                    root = rotateRight(root);
+                }
+            } else if(bf_root < -1) {
+                int bf_right = balance_factor(root->right);
+                if(bf_right <= 0) { //L0 and L-1 case
+                    root = rotateLeft(root);
+                } else {
+                    root->right = rotateRight(root->right);
+                    root = rotateLeft(root);
+                }
+            }
+        }
+    }
+    return root;
+}
+
+void avlDelete(AVL *tree, int key) {
+    tree->root = delete(tree->root, key);
+}
+
+void inorder(NODE *root) {
+    if(root == NULL) {
+        return;
+    }
+    inorder(root->left);
+    printf("%d ", root->key);
+    inorder(root->right);
+}
+
+void avlInorder(AVL *tree) {
+    inorder(tree->root);
+}
+
+int main() {
+    AVL *tree = newAVLTree();
+    printf("avlInsert %d\n",12);
+    avlInsert(tree, 12);
+    printf("height : %d\nkey : %d\n\n", tree->root->height, tree->root->key);
+
+    printf("avlInsert %d\n",90);
+    avlInsert(tree, 90);
+    printf("height : %d\nkey : %d\n\n", tree->root->height, tree->root->key);
+
+    printf("avlInsert %d\n",8);
+    avlInsert(tree, 8);
+    printf("height : %d\nkey : %d\n\n", tree->root->height, tree->root->key);
+
+    printf("avlInsert %d\n",20);
+    avlInsert(tree, 20);
+    printf("height : %d\nkey : %d\n\n", tree->root->height, tree->root->key);
+
+    printf("avlInsert %d\n",13);
+    avlInsert(tree, 13);
+    printf("height : %d\nkey : %d\n\n", tree->root->height, tree->root->key);
+
+    printf("avlInsert %d\n",15);
+    avlInsert(tree, 15);
+    printf("height : %d\nkey : %d\n\n", tree->root->height, tree->root->key);
+
+    printf("avlInsert %d\n",19);
+    avlInsert(tree, 19);
+    printf("height : %d\nkey : %d\n\n", tree->root->height, tree->root->key);
+
+    printf("avlInsert %d\n",62);
+    avlInsert(tree, 62);
+    printf("height : %d\nkey : %d\n\n", tree->root->height, tree->root->key);
     
-//     avlInsertion(t, 12);
-//     avlInsertion(t, 90);
-//     avlInsertion(t, 8);
-//     avlInsertion(t, 20);
-//     avlInsertion(t, 13);
-//     avlInsertion(t, 15);
-//     avlInsertion(t, 19);
-//     avlInsertion(t, 62);
-//     avlInsertion(t, 70);
-//     avlInsertion(t, 45);
+    printf("avlInsert %d\n",70);
+    avlInsert(tree, 70);
+    printf("height : %d\nkey : %d\n\n", tree->root->height, tree->root->key);
 
-//     inorderAVLTraversal(t);
-//     printf("\n");
+    printf("avlInsert %d\n",45);
+    avlInsert(tree, 45);
+    printf("height : %d\nkey : %d\n\n", tree->root->height, tree->root->key);
 
-//     avlDeletion(t, 90);
-//     avlDeletion(t, 20);
-//     avlDeletion(t, 8);
+    avlInsert(tree, 43);
+    avlInsert(tree, 30);
+    avlInsert(tree, 34);
+    avlInsert(tree, 67);
+    avlInsert(tree, 100);
 
-//     inorderAVLTraversal(t);
-//     printf("\n");
+    avlInorder(tree);
+    printf("\n");
 
-//     return 0;
-// }
+    avlDelete(tree, 43);
+    avlDelete(tree, 19);
+    avlDelete(tree, 8);
+    avlDelete(tree, 100);
+
+    avlInorder(tree);
+    printf("\n");
+    return 0;
+}
